afd177a3a4b255f1df57e6d436653eb35f28ceec
MrksDllngr <markus.dallinger89@gmail.com>
2021-03-17 16:58:05 +0100

    added second page

---
+++ pages/testpage.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { fetchTestContent } from "../helpers/helpers";
+import Layout from "../components/LayoutComponents/Layout";
+
+
+//example of styled components using Theme
+const Title = styled.h1`
+  color: ${(props) =>
+    props.primary ? props.theme.colors.main1 : props.theme.colors.main2};
+  text-align: center;
+`;
+
+export default function Home(props) {
+  
+const {content, preview} = props;
+
+  return (
+    <Layout preview={preview}>
+      <Title primary >{content[0].text}</Title>
+      <p>this is another page</p>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+
+  const content = await fetchTestContent(preview);
+
+  return {
+    props: { content: content, preview: preview },
+    revalidate:60,
+  };
+}
